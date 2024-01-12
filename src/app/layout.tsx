c65cc59188bcee87402d018704c4f046a12cc8d5
João Melo <jopcmelo@gmail.com>
2024-01-12 12:24:06 -0300

    feat: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import './globals.css';
+import Footer from '@/components/footer';
 import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'], variable: '--body-font' });
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={cn('min-h-screen antialiased', inter.className)}>
         {children}
+        <Footer />
       </body>
     </html>
   );
